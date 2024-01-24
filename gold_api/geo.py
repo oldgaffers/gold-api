c3d277c223e9eb7f9735fd8e34e1a152bc2a5d41
jcable <julian.cable@bbc.co.uk>
2024-01-24 07:57:01 +0000

    fix: use bng_latlon

--- gold_api/geo.py
+++ gold_api/geo.py
@@ -1,13 +1,16 @@
 import requests
-import json
-from pyproj import Transformer
+from bng_latlon import OSGB36toWGS84
+import boto3
 
-key = '' # TODO get from SSM
+ssm = boto3.client('ssm')
+
+r = ssm.get_parameter(Name='/OS/API_KEY')
+key = r['Parameter']['Value']
 
 def find(place):
     r = requests.get('https://api.os.uk/search/names/v1/find', headers={'key': key}, params={'query': place, 'maxresults': 1})
     if r.ok:
         answer = r.json()
         entry = answer['results'][0]['GAZETTEER_ENTRY']
-        ll = Transformer.from_crs("epsg:27700", "epsg:4326").transform(entry['GEOMETRY_X'], entry['GEOMETRY_Y'])
+        ll = OSGB36toWGS84(entry['GEOMETRY_X'], entry['GEOMETRY_Y'])
         return { 'place': place, 'latitude': ll[0], 'longitude': ll[1] }
